815a9bda89cfe52ff2e293d11aa0070ff3eb6dcb
woonhahaha <woonhahaha@gmail.com>
2020-03-07 23:42:31 +0900

    chess

--- boj/IO/1100.cpp
+++ boj/IO/1100.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(void)
 {
-	char board[8][8];
+    char board[8][8];
     int i, j, total = 0;
 
     for (i = 0; i < 8; i++)
@@ -16,6 +16,8 @@ int main(void)
                 total += 1;
             else if (i % 2 == 1 && j % 2 == 1 && board[i][j] == 'F')
                 total += 1;
+
     cout << total;
+    
 	return 0;
 }
