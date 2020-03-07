8dc62c047bb315411aad02c63c84978671f780ff
woonhahaha <woonhahaha@gmail.com>
2020-03-07 23:41:31 +0900

    white part on chess board

---
+++ boj/IO/1100.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	char board[8][8];
+    int i, j, total = 0;
+
+    for (i = 0; i < 8; i++)
+        for (j = 0; j < 8; j++)
+            cin >> board[i][j];
+            
+    for (i = 0; i < 8; i++)
+        for (j = 0; j < 8; j++)
+            if (i % 2 == 0 && j % 2 == 0 && board[i][j] == 'F')
+                total += 1;
+            else if (i % 2 == 1 && j % 2 == 1 && board[i][j] == 'F')
+                total += 1;
+    cout << total;
+	return 0;
+}
