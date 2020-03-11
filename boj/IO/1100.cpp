73614b0954635104c7eeb0467b9810f169c91953
woonhahaha <woonhahaha@gmail.com>
2020-03-11 12:22:15 +0900

    white part of a chess board

--- boj/IO/1100.cpp
+++ boj/IO/1100.cpp
@@ -9,7 +9,7 @@ int main(void)
     for (i = 0; i < 8; i++)
         for (j = 0; j < 8; j++)
             cin >> board[i][j];
-            
+
     for (i = 0; i < 8; i++)
         for (j = 0; j < 8; j++)
             if (i % 2 == 0 && j % 2 == 0 && board[i][j] == 'F')
